5d7bad463298f4ec238b27052b1f5b26318795b9
Alireza <127423933+alirezaalamshah@users.noreply.github.com>
2025-05-13 15:57:17 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // ✅ مسیر درست برای GitHub Pages
+  base: '/', // 
   plugins: [react()],
 })
